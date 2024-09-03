8b8fe56caa6bcc485a343d5428ff61db9e041a15
gluke0 <milanesigianluca@gmail.com>
2024-09-03 14:05:05 +0200

    removed italic

--- main.js
+++ main.js
@@ -137,7 +137,7 @@ buttons.forEach(button=>{
         <section class="container-sm p-3">
           <span class="mt-2 font-monospace">${receipt.outerHTML}</span>
           <br>
-          <span class="text-center fst-italic font-monospace"> ${toPay.outerHTML} </span>
+          <span class="text-center font-monospace"> ${toPay.outerHTML} </span>
         </section>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script
         </body>
