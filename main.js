3da6d934b09229437ae9beeec17fa008631eadcf
gluke0 <milanesigianluca@gmail.com>
2024-09-03 11:40:44 +0200

    added some padding

--- main.js
+++ main.js
@@ -134,7 +134,7 @@ buttons.forEach(button=>{
            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
         </head>
         <body>
-        <section class="container-sm">
+        <section class="container-sm p-3">
           <span class="mt-2">${receipt.outerHTML}</span>
           <br>
           <span class="text-center fst-italic"> ${toPay.outerHTML} </span>
