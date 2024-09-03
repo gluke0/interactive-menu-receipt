b79f9d616716ec47897be0b90e39747ae506f22d
gluke0 <milanesigianluca@gmail.com>
2024-09-03 11:35:24 +0200

    more style

--- main.js
+++ main.js
@@ -137,7 +137,7 @@ buttons.forEach(button=>{
         <section class="container-sm">
           <span class="mt-2">${receipt.outerHTML}</span>
           <br>
-          <span class="text-center"> ${toPay.outerHTML} </span>
+          <span class="text-center fst-italic"> ${toPay.outerHTML} </span>
         </section>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script
         </body>
