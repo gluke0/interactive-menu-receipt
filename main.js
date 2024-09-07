2dd4001f9638def034975030eca7a1286e5bca25
gluke0 <milanesigianluca@gmail.com>
2024-09-07 16:24:30 +0200

    notes is in the receipt now

--- main.js
+++ main.js
@@ -140,11 +140,9 @@ buttons.forEach(button=>{
             <span class="mt-2 font-monospace">${receipt.outerHTML}</span>
             <br>
             <span class="text-center font-monospace"> ${toPay.outerHTML} </span>
+            <br>
+            <span class="text-center font-monospace"> ${notes.outerHTML} </span>
           </div>
-
-          <div> 
-            <span> ${notes.outerHTML} </span>
-          </div>  
 
         </section>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script
